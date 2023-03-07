61c7e824ed9400ff4811eab059c400c627929999
Maria Micioni <m.micioni85@gmail.com>
2023-03-07 18:43:53 +0100

    models_validation

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,8 @@ class Activity < ApplicationRecord
   has_many :feedback
   has_many :activities_notes
   has_many_attached :photos
+  validates :address, presence: true
+  validates :categories, presence: true
+  validates :price, presence: true
+  validates :duration, presence: true
 end
