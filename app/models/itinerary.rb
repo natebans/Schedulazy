539af88accc53bbb23b155e309978c58b67dc8d5
Maria Micioni <m.micioni85@gmail.com>
2023-03-08 06:50:33 +0100

    added pg search gem

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,6 +1,11 @@
 class Itinerary < ApplicationRecord
+  include PgSearch::Model
   belongs_to :user
   has_many :activities
   has_many :itinerary_notes
   has_many_attached :photos
+  pg_search_scope :search_name, against: [:name, :description],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
