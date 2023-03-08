c5a5305c7c30c1c7162ef8c6f1498dc4b660e805
Maria Micioni <m.micioni85@gmail.com>
2023-03-08 09:45:05 +0100

    search on homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_133403) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_083832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_133403) do
     t.bigint "itinerary_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
     t.index ["itinerary_id"], name: "index_activities_on_itinerary_id"
   end
 
@@ -82,6 +83,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_133403) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "max_budget"
     t.index ["user_id"], name: "index_itineraries_on_user_id"
   end
 
