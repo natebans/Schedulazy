6eeb74471515dc0cb3a874bc36c26237d50b82bd
Nate Banas <nate.banas@gmail.com>
2023-03-10 20:04:31 +0100

    connected swup to site, added stimulus controller, added buttons to navigate pages

---
+++ app/javascript/controllers/swup_controller.js
@@ -0,0 +1,9 @@
+import { Controller } from "@hotwired/stimulus"
+import Swup from 'swup'
+
+// Connects to data-controller="swup"
+export default class extends Controller {
+  connect() {
+    console.log("swup controller is connected")
+  }
+}
